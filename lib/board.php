b996270351d5b229c27a9da3f3bdb59f066d5374
Alexandros Magos <alexandrosmagos@hotmail.com>
2022-01-20 20:01:42 +0200

    Logic Procedure removed from DB and integrated into game
    - Big and messy logic procedure removed and integrated into board.php
    - Schema cleaned and comments translated into English

--- lib/board.php
+++ lib/board.php
@@ -7,7 +7,6 @@ function reset_board() {
 	$mysqli->query($sql);
 }
 
-//Έλεγχοι πρωτού κάνει την κίνηση
 function make_move($choice,$player_number,$token) {
 	
 	if($token==null || $token=='') {
@@ -42,10 +41,115 @@ function make_move($choice,$player_number,$token) {
 //Καλεί την stored precedure που κάνει την κίνηση του παίχτη
 function do_move($choice,$player_number) {
 	global $mysqli;
-	$sql = 'call `make_move`(?,?);';
+
+	if ($player_number=='p1') {
+		$sql = "UPDATE `board` SET p1_choice=? WHERE match_id=(SELECT MAX(match_id) FROM board);";
+		$opponent = 'p2';
+	} else {
+		$sql = "UPDATE `board` SET p2_choice=? WHERE match_id=(SELECT MAX(match_id) FROM board);";
+		$opponent = 'p1';
+	}
 	$st = $mysqli->prepare($sql);
-	$st->bind_param('is',$choice,$player_number);
+	$st->bind_param('i',$choice);
 	$st->execute();
+
+
+	$sql = 'UPDATE `game_status` set player_turn=?;';
+	$st = $mysqli->prepare($sql);
+	$st->bind_param('s',$opponent);
+	$st->execute();
+
+	check_winner();
+
+}
+
+function check_winner() {
+	global $mysqli;
+	$st3=$mysqli->prepare('select count(*) as choices from board WHERE p1_choice is not null and p2_choice is not null');
+	$st3->execute();
+	$res3 = $st3->get_result();
+	$choices = $res3->fetch_assoc()['choices'];
+	if($choices>0) {
+		$sql = 'select * from board';
+		$st = $mysqli->prepare($sql);
+		$st->execute();
+		$res = $st->get_result();
+		$board = $res->fetch_assoc();
+
+
+		$winner=null; 
+		$winner_text=null;
+
+		if ($board['p1_choice'] != $board['p2_choice']) {
+
+			if ($board['p1_choice']==1) {
+				if ($board['p2_choice']==3){
+					$winner='p1'; $winner_text='Rock crushes Scissors';
+				} else if ($board['p2_choice']==4){
+					$winner='p1'; $winner_text='';
+				} else if ($board['p2_choice']==2){
+					$winner='p2'; $winner_text='Paper covers Rock';
+				} else if($board['p2_choice']==5){
+					$winner='p2'; $winner_text='Spock vaporizes Rock';
+				}
+			}
+
+			if ($board['p1_choice']==2) {
+				if ($board['p2_choice']==3){
+					$winner='p2'; $winner_text='Scissors cuts Paper';
+				} else if ($board['p2_choice']==4){
+					$winner='p2'; $winner_text='Lizard eats Paper';
+				} else if ($board['p2_choice']==1){
+					$winner='p1'; $winner_text='Paper covers Rock';
+			}
+			}
+
+			if ($board['p1_choice']==3) {
+				if ($board['p2_choice']==2){
+					$winner='p1'; $winner_text='Scissors cuts Paper';
+				} else if ($board['p2_choice']==4){
+					$winner='p1'; $winner_text='Scissors decapitates Lizard';
+				} else if ($board['p2_choice']==1){
+					$winner='p2'; $winner_text='Rock crushes Scissors';
+				} else if($board['p2_choice']==5){
+					$winner='p2'; $winner_text='Spock smashes Scissors';
+				}
+			}
+
+			if ($board['p1_choice']==4) {
+				if ($board['p2_choice']==1){
+					$winner='p2'; $winner_text='Rock crushes Lizard';
+				} else if ($board['p2_choice']==3){
+					$winner='p2'; $winner_text='Scissors decapitates Lizard';
+				} else if ($board['p2_choice']==2){
+					$winner='p1'; $winner_text='Lizard eats Paper';
+				} else if($board['p2_choice']==5){
+					$winner='p1'; $winner_text='Lizard poisons Spock';
+				}
+			}
+
+			if ($board['p1_choice']==5) {
+				if ($board['p2_choice']==1){
+					$winner='p1'; $winner_text='Spock vaporizes Rock';
+				} else if ($board['p2_choice']==3){
+					$winner='p1'; $winner_text='Spock smashes Scissors';
+				} else if ($board['p2_choice']==2){
+					$winner='p2'; $winner_text='Paper disproves Spock';
+				} else if($board['p2_choice']==4){
+					$winner='p2'; $winner_text='Lizard poisons Spock';
+				}
+			}
+
+		} else {
+			$winner='D'; $winner_text='';
+		}
+
+		$sql = "UPDATE game_status SET result=?, result_text=?, status='ended'";
+		$st = $mysqli->prepare($sql);
+		$st->bind_param('ss',$winner,$winner_text);
+		$st->execute();
+
+	}
 }
 
 //Καλεί την stored precedure που ετοιμάζει την βάση για δεύτερο παιχνίδι με τους ίδιους παίχτες
--- schema.sql
+++ schema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2022 at 08:11 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.3.29
+-- Generation Time: Jan 20, 2022 at 06:52 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -23,118 +23,19 @@ SET time_zone = "+00:00";
 
 DELIMITER $$
 --
--- Procedures
+-- Procedure clean_board: We initialize the board through replace with board_empty
 --
 DROP PROCEDURE IF EXISTS `clean_board`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `clean_board` ()  BEGIN
 		TRUNCATE TABLE board;
 		REPLACE INTO `board` SELECT * FROM `board_empty`;
         UPDATE `players` SET username=NULL, token=NULL, last_action=NULL;
-		UPDATE `game_status` SET `status`='not active', `player_turn`=NULL, `result_text`=NULL , `result`=NULL;
+		UPDATE `game_status` SET `status`='not active', `player_turn`=NULL, `result_text`=NULL ,`result`=NULL;
 	END$$
 
-
-DROP PROCEDURE IF EXISTS `make_move`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `make_move`(IN `choice` TINYINT, IN `player_number` VARCHAR(10))
-BEGIN
-        DECLARE player1 INT;
-        DECLARE player2 INT;
-        
-		IF (player_number LIKE 'p1') THEN
-		UPDATE `board` SET p1_choice=choice WHERE match_id=(SELECT MAX(match_id) FROM board);
-		END IF;
-		
-		IF (player_number LIKE 'p2') THEN
-		UPDATE `board` SET p2_choice=choice WHERE match_id=(SELECT MAX(match_id) FROM board);
-		END IF;
-		
-		UPDATE game_status SET player_turn=IF(player_number='p1','p2','p1');
-		
-        
-		SELECT p1_choice INTO player1 FROM `board`;
-		SELECT p2_choice INTO player2 FROM `board`;
-
-    IF (player1 IS NOT NULL AND player2 IS NOT NULL) THEN
-    
-    	IF player1=1 THEN
-			IF player2=3 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Rock crushes Scissors', `status`='ended';
-			ELSEIF player2=4 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Rock crushes Lizard', `status`='ended';
-			
-			ELSEIF player2=2 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Paper covers Rock', `status`='ended';
-			
-			ELSEIF player2=5 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Spock vaporizes Rock', `status`='ended';
-			END IF;
-    	END IF;
-        
-    	IF player1=2 THEN
-			IF player2=3 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Scissors cuts Paper', `status`='ended';
-			
-			ELSEIF player2=4 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Lizard eats Paper', `status`='ended';
-			
-			ELSEIF player2=1 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Paper covers Rock', `status`='ended';
-			END IF;
-    	END IF;
-        
-        IF player1=3 THEN
-			IF player2=2 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Scissors cuts Paper', `status`='ended';
-			
-			ELSEIF player2=4 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Scissors decapitates Lizard', `status`='ended';
-			
-			ELSEIF player2=1 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Rock crushes Scissors', `status`='ended';
-			
-			ELSEIF player2=5 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Spock smashes Scissors', `status`='ended';
-			END IF;
-    	END IF;
-        
-        IF player1=4 THEN
-			IF player2=1 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Rock crushes Lizard', `status`='ended';
-			
-			ELSEIF player2=3 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Scissors decapitates Lizard', `status`='ended';
-			
-			ELSEIF player2=2 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Lizard eats Paper', `status`='ended';
-			
-			ELSEIF player2=5 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Lizard poisons Spock', `status`='ended';
-			END IF;
-    	END IF;
-        
-	IF player1=5 THEN
-			IF player2=1 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Spock vaporizes Rock', `status`='ended';
-			
-			ELSEIF player2=3 THEN
-				UPDATE `game_status` SET `result`='p1', result_text='Spock smashes Scissors', `status`='ended';
-			
-			ELSEIF player2=2 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Paper disproves Spock', `status`='ended';
-			
-			ELSEIF player2=4 THEN
-				UPDATE `game_status` SET `result`='p2', result_text='Lizard poisons Spock', `status`='ended';
-			END IF;
-    	END IF;
-        
-	IF player1=player2 THEN
-		UPDATE `game_status` SET `result`='D', `status`='ended';
-	END IF;
-        
-    END IF;
-    END$$
-
-
+--
+-- Procedure play_again: We initialize the board through replace with board_empty while keeping current players in game
+--
 DROP PROCEDURE IF EXISTS `play_again`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `play_again` ()  BEGIN
 
@@ -153,7 +54,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `play_again` ()  BEGIN
 		
 	END$$
 
-
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -161,7 +61,7 @@ DELIMITER ;
 --
 -- Table structure for table `board`
 --
-DROP TABLE IF EXISTS `board`;
+DROP TABLE IF EXISTS board;
 CREATE TABLE `board` (
   `match_id` mediumint(9) NOT NULL,
   `p1_choice` char(10) DEFAULT NULL,
@@ -171,6 +71,7 @@ CREATE TABLE `board` (
 --
 -- Dumping data for table `board`
 --
+
 INSERT INTO `board` (`match_id`, `p1_choice`, `p2_choice`) VALUES
 (1, NULL, NULL);
 
@@ -179,7 +80,7 @@ INSERT INTO `board` (`match_id`, `p1_choice`, `p2_choice`) VALUES
 --
 -- Table structure for table `board_empty`
 --
-DROP TABLE IF EXISTS `board_empty`;
+DROP TABLE IF EXISTS board_empty;
 CREATE TABLE `board_empty` (
   `match_id` mediumint(9) NOT NULL,
   `p1_choice` char(10) DEFAULT NULL,
@@ -189,6 +90,7 @@ CREATE TABLE `board_empty` (
 --
 -- Dumping data for table `board_empty`
 --
+
 INSERT INTO `board_empty` (`match_id`, `p1_choice`, `p2_choice`) VALUES
 (1, NULL, NULL);
 
@@ -197,26 +99,27 @@ INSERT INTO `board_empty` (`match_id`, `p1_choice`, `p2_choice`) VALUES
 --
 -- Table structure for table `game_status`
 --
-DROP TABLE IF EXISTS `game_status`;
+DROP TABLE IF EXISTS game_status;
 CREATE TABLE `game_status` (
   `status` enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
   `player_turn` enum('p1','p2') DEFAULT NULL,
   `result` enum('p1','p2','D') DEFAULT NULL,
-  `result_text` CHAR(50) DEFAULT NULL,
+  `result_text` char(50) DEFAULT NULL,
   `last_change` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `game_status`
 --
+
 INSERT INTO `game_status` (`status`, `player_turn`, `result`, `result_text`, `last_change`) VALUES
-('not active', NULL, NULL, NULL, '2022-01-15 19:11:09');
+('not active', NULL, NULL, NULL, '2022-01-20 17:38:27');
 
 --
--- Triggers `game_status`
+-- Triggers `game_status`: Before each game_status table update, update the last_change column with the current time
 --
 DELIMITER $$
-DROP TRIGGER IF EXISTS `game_status_update`;
+DROP TRIGGER IF EXISTS game_status_update;
 CREATE TRIGGER `game_status_update` BEFORE UPDATE ON `game_status` FOR EACH ROW BEGIN
 		SET NEW.last_change=NOW();
 	END
@@ -228,7 +131,7 @@ DELIMITER ;
 --
 -- Table structure for table `players`
 --
-DROP TABLE IF EXISTS `players`;
+DROP TABLE IF EXISTS players;
 CREATE TABLE `players` (
   `username` varchar(50) DEFAULT NULL,
   `player_number` enum('p1','p2') NOT NULL,
@@ -239,6 +142,7 @@ CREATE TABLE `players` (
 --
 -- Dumping data for table `players`
 --
+
 INSERT INTO `players` (`username`, `player_number`, `token`, `last_action`) VALUES
 (NULL, 'p1', '', NULL),
 (NULL, 'p2', '', NULL);
